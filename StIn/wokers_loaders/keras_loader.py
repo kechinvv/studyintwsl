42584697a157bf87e3d83256cd0f5b78cb94a932
valera <kechinv1@rambler.ru>
2023-05-04 15:53:40 +0400

    lstm model, keras loader update

--- StIn/wokers_loaders/keras_loader.py
+++ StIn/wokers_loaders/keras_loader.py
@@ -1,8 +1,17 @@
 import threading
 
+import numpy as np
 import tensorflow as tf
+from keras.preprocessing import text
+from tensorflow.python.keras.engine.input_layer import InputLayer
+from keras.utils.data_utils import pad_sequences
+
 
 from StIn import res_dict
+
+max_features = 5000
+
+tokenizer = text.Tokenizer(filters='[]\'\" ', split=',', num_words=max_features)
 
 
 def keras_creator(work):
@@ -12,7 +21,18 @@ def keras_creator(work):
 
 def keras_predictor(model, dtw):
     try:
-        preds = model.predict(dtw)
-        res_dict[threading.current_thread().name] = (preds, "ok")
+        maxlen = 60
+
+        for layer in model.layers:
+            if type(layer) == InputLayer:
+                maxlen = layer.input_shape[0][1]
+
+        list_tokenized = tokenizer.texts_to_sequences([dtw])
+        input_dtw = pad_sequences(list_tokenized, maxlen=maxlen)
+
+        preds = model.predict(input_dtw)
+
+        max_res_ind = np.unravel_index(np.argmax(preds, axis=None), preds.shape)
+        res_dict[threading.current_thread().name] = (max_res_ind, "ok")
     except Exception as e:
         res_dict[threading.current_thread().name] = ("", str(e))
